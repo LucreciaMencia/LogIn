6114d383eeb5d698df2a3700772be5ea8165e327
LeoCzaj <czajkoleonardo@gmail.com>
2025-10-27 16:14:35 -0300

    Update page.tsx

--- LogIn/Frontend/src/app/register/page.tsx
+++ LogIn/Frontend/src/app/register/page.tsx
@@ -28,7 +28,7 @@ export default function RegisterPage() {
       return;
     }
 
-    const response = await fetch("http://localhost:3000/register", {
+    const response = await fetch("http://localhost:8000/register", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
@@ -87,3 +87,4 @@ export default function RegisterPage() {
     </div>
   );
 }
+
