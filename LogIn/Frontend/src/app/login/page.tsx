0e9830462e0760f8c245e2bf72aa2da986eb779d
LeoCzaj <czajkoleonardo@gmail.com>
2025-10-27 16:50:13 -0300

    Update page.tsx

--- LogIn/Frontend/src/app/login/page.tsx
+++ LogIn/Frontend/src/app/login/page.tsx
@@ -30,7 +30,7 @@ export default function Login() {
   const handleLogin = async (e) => {
     e.preventDefault();
 
-    const response = await fetch("https://alluringly-uncategorical-kristen.ngrok-free.dev/login", {
+    const response = await fetch("http://localhost:8000/register", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password })
