0da7fd421fb551a2cc2ff836325972f9f2c7057b
LeoCzaj <czajkoleonardo@gmail.com>
2025-10-28 13:40:23 -0300

    Update page.tsx

--- LogIn/Frontend/src/app/login/page.tsx
+++ LogIn/Frontend/src/app/login/page.tsx
@@ -30,7 +30,7 @@ export default function Login() {
   const handleLogin = async (e) => {
     e.preventDefault();
 
-    const response = await fetch("http://localhost:8000/register", {
+    const response = await fetch("http://localhost:3000/register", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password })
