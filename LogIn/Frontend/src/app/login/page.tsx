fcc9c197dce23884841c1a018514f17583c5c914
Gyldra <czajkoleonardo@gmail.com>
2025-11-03 14:54:49 -0300

    Update page.tsx

--- LogIn/Frontend/src/app/login/page.tsx
+++ LogIn/Frontend/src/app/login/page.tsx
@@ -30,7 +30,7 @@ export default function Login() {
   const handleLogin = async (e) => {
     e.preventDefault();
 
-    const response = await fetch("http://localhost:3000/register", {
+    const response = await fetch("http://localhost:3000/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password })
